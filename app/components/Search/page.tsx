87459cd697e12d4eecb1da1da21c6821671e1ebb
Alapan Das <alapandas333@gmail.com>
2024-07-07 12:57:28 +0530

    Home Page

--- app/components/Search/page.tsx
+++ app/components/Search/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Combbox from '../Combbox/page.jsx'
 
 type Props = {}
 
@@ -9,7 +8,7 @@ export default function Search({ }: Props) {
       <div>
         <h1 className='font-bold text-3xl text-heading font-poppins mx-6'>Search</h1>
       </div>
-      <Combbox />
+    
 
     </div>
   )
